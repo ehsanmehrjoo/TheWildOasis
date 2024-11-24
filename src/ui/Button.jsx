d7dda580d67622c8cd9791fabd0828f8af183174
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-11-24 18:13:10 +0330

    Update Button

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -54,10 +54,12 @@ const Button = styled.button`
   font-weight: 500;
   border: none;
   border-radius: var(--border-radius-sm);
-  background-color: var(--color-brand-500 );
+  background-color: var(--color-brand-600 );
   color: var(--color-brand-50 );
   box-shadow: var(--shadow-sm);
   cursor: pointer;
- 
+  &:hover{
+    background-color: var(--color-brand-700 );
+  }
 `;
 export default Button
