856350bef6601007081bb1f15f2dcbac237ff30f
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-11-24 18:07:11 +0330

    Update Input

--- src/App.jsx
+++ src/App.jsx
@@ -64,13 +64,7 @@ const H1 = styled.h1`
 
  
 
-const Input = styled.input`
-  border: 1px solid  var(--color-grey-300);
-  background-color: var(--color-grey-0);
-  border-radius: var(--border-radius-sm);
-  padding: 0.8rem 1.2rem;
-  box-shadow: var(--shadow-sm);
-`;
+
 
 // const AppContainer = styled.div`
 //   display: flex;
