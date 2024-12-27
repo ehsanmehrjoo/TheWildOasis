b9822ac07b4e0f2210f9c4824170647d9b4d0cda
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-12-27 10:54:35 +0330

    Update UpdateSettingsForm

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,8 +1,10 @@
 import Form from '../../ui/Form';
 import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
+import useSettings from './useSettings';
 
 function UpdateSettingsForm() {
+  const {settings , isLoading , error } = useSettings()
   return (
     <Form>
       <FormRow label='Minimum nights/booking'>
