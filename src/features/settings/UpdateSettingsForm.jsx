465b15f23c9105772ea1e720e0906357c5caaa4e
ehsanmehrjoo <ehsan585147z@gmail.com>
2024-12-27 12:01:52 +0330

    Update UpdateSettingsForm and Setting

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,38 +1,64 @@
- 
 import Form from '../../ui/Form';
 import FormRow from '../../ui/FormRow';
 import Input from '../../ui/Input';
 import Spinner from '../../ui/Spinner';
 import useSettings from './useSettings';
-import useUpdateSetting from './useUpdateSetting';
+import useUpdateSetting from '../settings/useUpdateSetting';
 
 function UpdateSettingsForm() {
   const { settings, isLoading, error } = useSettings();
+  const { updateSetting , isUpdating } = useUpdateSetting(); // هوک را قبل از هر شرط فراخوانی کنید
 
   if (isLoading) return <Spinner />;
   if (error) return <p>Error loading settings.</p>;
   if (!settings) return null;
 
   const { minBookingLength, maxBookingLength, maxGuestsPerBooking, breakfastPrice } = settings;
- 
-  const { updateSetting, isUpdating } = useUpdateSetting();
-  
-  function handelUpdate(e){
+
+  function handelUpdate(e, field) {
     e.preventDefault();
+    const { value } = e.target;
+    if (!value) return;
+    updateSetting({ [field]: value });
   }
+
   return (
-    <Form >
+    <Form>
       <FormRow label="Minimum nights/booking">
-        <Input type="number" id="min-nights" defaultValue={minBookingLength} onBlur={(e) => handelUpdate(e, 'minBookingLength')}/>
+        <Input
+          type="number"
+          id="min-nights"
+        disabled={isUpdating}
+          defaultValue={minBookingLength}
+          onBlur={(e) => handelUpdate(e, 'minBookingLength')}
+        />
       </FormRow>
       <FormRow label="Maximum nights/booking">
-        <Input type="number" id="max-nights" defaultValue={maxBookingLength} />
+        <Input
+          type="number"
+          id="max-nights"
+        disabled={isUpdating}
+          defaultValue={maxBookingLength}
+          onBlur={(e) => handelUpdate(e, 'maxBookingLength')}
+        />
       </FormRow>
       <FormRow label="Maximum guests/booking">
-        <Input type="number" id="max-guests" defaultValue={maxGuestsPerBooking} />
+        <Input
+          type="number"
+          id="max-guests"
+        disabled={isUpdating}
+          defaultValue={maxGuestsPerBooking}
+          onBlur={(e) => handelUpdate(e, 'maxGuestsPerBooking')}
+        />
       </FormRow>
       <FormRow label="Breakfast price">
-        <Input type="number" id="breakfast-price" defaultValue={breakfastPrice} />
+        <Input
+          type="number"
+          id="breakfast-price"
+        disabled={isUpdating}
+          defaultValue={breakfastPrice}
+          onBlur={(e) => handelUpdate(e, 'breakfastPrice')}
+        />
       </FormRow>
     </Form>
   );
