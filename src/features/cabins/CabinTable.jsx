86c5429176d0ace11ea65fd9a042652ebcb6f256
ehsanmehrjoo <ehsan585147z@gmail.com>
2025-01-14 12:49:56 +0330

    Update CabinTableOperations and Filter and CabinTable and Cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -5,6 +5,7 @@ import CabinRow from "./CabinRow";
 import useCabins from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
+import { useSearchParams } from "react-router-dom";
 
 // استایل جدول
 // const Table = styled.div`
@@ -24,31 +25,45 @@ import Menus from "../../ui/Menus";
 // `;
 
 // استایل هدر جدول
-const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
+// const TableHeader = styled.header`
+//   display: grid;
+//   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+//   column-gap: 2.4rem;
+//   align-items: center;
 
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.4rem;
+//   background-color: var(--color-grey-50);
+//   border-bottom: 1px solid var(--color-grey-100);
+//   text-transform: uppercase;
+//   letter-spacing: 0.4px;
+//   font-weight: 600;
+//   color: var(--color-grey-600);
+//   padding: 1.6rem 2.4rem;
 
-  @media (max-width: 768px) {
-    grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr; /* حفظ ساختار گرید */
-    column-gap: 1.4rem;
-    background-color: var(--color-grey-50);
-    width: 100%;
-  }
-`;
+//   @media (max-width: 768px) {
+//     grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr; /* حفظ ساختار گرید */
+//     column-gap: 1.4rem;
+//     background-color: var(--color-grey-50);
+//     width: 100%;
+//   }
+// `;
 
 function CabinTable() {
-  // استفاده صحیح از هوک useCabins
+
   const { isLoading, cabins, error } = useCabins();
+  const [searchParams ]  = useSearchParams()
+
+
+  const filterValue = searchParams.get('discount') || 'all';
+  let filterCabin ;
+  if (filterValue === 'all') {
+    filterCabin = cabins
+  }
+  if(filterValue === 'no-discount'){
+    filterCabin  = cabins.filter((no) => !no.discount)
+  }
+  if(filterValue === 'with-discount'){
+    filterCabin = cabins.filter((cabin) => cabin.discount);
+  }
 
   // نمایش اسپینر هنگام بارگذاری
   if (isLoading) return <Spinner />;
@@ -69,7 +84,7 @@ function CabinTable() {
         <div></div>
       </Table.Header>
       <Table.Body
-       data={cabins} 
+       data={filterCabin} 
        render={(cabin) => (
         <CabinRow cabin={cabin} key={cabin.id} />
   )}>
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -3,6 +3,7 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
 import AddCabin from "../features/cabins/AddCabin";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
  
 
 function Cabins() {
@@ -12,7 +13,7 @@ function Cabins() {
   return ( <>
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
-      <p>Filter / Sort</p>
+      <CabinTableOperations/>
      
     </Row>
     <Row>
