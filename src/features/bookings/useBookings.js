d76489eb0471003e6dde6ba425fd53736beda085
ehsanmehrjoo <ehsan585147z@gmail.com>
2025-01-23 22:43:17 +0330

    Update useBookings and apiBookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,10 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 function useBookings() {
+  const [searchparams] = useSearchParams();
+// Filter
+const filterValue = searchparams.get('status')
+const filter = !filterValue || filterValue === "all" ? null :
+ {field : "status" , value : filterValue};
+// {field : "totalPrice" , value : 5000 , method : "gte" };
+
+// Sort by
+const sortValue = searchparams.get('sortBy')
+// const sortBy = !sortValue || sortValue === "startDate-desc" ? null : ""
+
   const { isLoading, data: bookings = [], error } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({filter}),
   });
 
   console.log(bookings);
