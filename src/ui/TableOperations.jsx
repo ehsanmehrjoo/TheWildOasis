88401ea0deca7d74d3e437ea1cb24d38727a4569
ehsanmehrjoo <ehsan585147z@gmail.com>
2025-02-10 12:55:54 +0330

    Update TableOperations

--- src/ui/TableOperations.jsx
+++ src/ui/TableOperations.jsx
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
 
 
-export default TableOperations;
 const TableOperations = styled.div`
   display: flex;
   align-items: center;
   gap: 1.6rem;
   flex-wrap: wrap; /* Allow items to wrap to the next line */
-
+  
   @media (max-width: 768px) {
     justify-content: center; /* وسط‌چین کردن در صفحه‌های کوچک */
   }
-`;
+  `;
+  export default TableOperations;
